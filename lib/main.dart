ff8272a58aabd351dbfde3652f7f1c08e8475015
Amanda Cleto <amanda.cleto.dev@gmail.com>
2023-01-16 17:14:45 -0300

    feat: displayed my rive house into the home page; added rive lib to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const UsingRive());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class UsingRive extends StatelessWidget {
+  const UsingRive({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text("Hello World Niluka"),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rive'),
+      ),
+      body: Center(
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 500,
+              width: 300,
+              child: RiveAnimation.asset(
+                'assets/riv/new_file.riv',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
